c55e6b0281a386b25b881bc47332ab612622c141
Gustavo Bellini Bigardi <gbbigardi@bigardi-a60muv>
2022-09-23 12:12:56 -0300

    init

---
+++ src/Keda.ServiceBus.Consumer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+using Azure.Messaging.ServiceBus;
+
+using Microsoft.Azure.ServiceBus;
+
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+var logger = new LoggerConfiguration()
+    .WriteTo.Console(theme: AnsiConsoleTheme.Literate)
+    .CreateLogger();
+
+logger.Information("Consumindo Topico do Azure Service Bus...");
+
+string connectionString = "Endpoint=sb://festivaltecnologiams.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dK+QoNjL9ip8IUMqmwnq8/YhJHVtPTZa4zoMWwYK/uw=;";
+string topic = "demo";
+string subscription = "SubDemo";
+
+ServiceBusClient? client = null;
+ServiceBusProcessor? processor = null;
+
+try
+{
+    client = new ServiceBusClient(connectionString);
+    processor = client.CreateProcessor(topic, subscription, new ServiceBusProcessorOptions());
+
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    while (true)
+    {
+        Thread.Sleep(1000);
+    }
+}
+catch (Exception ex)
+{
+    logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                    $"Mensagem: {ex.Message}");
+}
+finally
+{
+    if (client is not null)
+    {
+        await client.DisposeAsync();
+        logger.Information(
+            "Conexao com o Azure Service Bus fechada!");
+    }
+        
+}
+
+static async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+static Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
---
+++ src/Keda.ServiceBus.Producer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+using Microsoft.Azure.ServiceBus;
+
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+var logger = new LoggerConfiguration()
+    .WriteTo.Console(theme: AnsiConsoleTheme.Literate)
+    .CreateLogger();
+
+logger.Information("Enviando Topico do Azure Service Bus...");
+
+string connectionString = "Endpoint=sb://festivaltecnologiams.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dK+QoNjL9ip8IUMqmwnq8/YhJHVtPTZa4zoMWwYK/uw=;";
+string topic = "demo";
+
+if (args.Length == 0 || !int.TryParse(args[0], out int quantity))
+{
+    logger.Information("Argumento de quantidade de mensagens inválido");
+    return;
+}
+
+TopicClient? client = null;
+
+try
+{
+    client = new TopicClient(connectionString, topic);
+
+    for (int i = 1; i <= quantity; i++)
+    {
+        await client.SendAsync(
+            new Message(Encoding.UTF8.GetBytes($"Mensagem {i}")));
+
+        logger.Information(
+            $"[Mensagem enviada] {i}");
+    }
+
+    logger.Information("Concluido o envio de mensagens");
+}
+catch (Exception ex)
+{
+    logger.Error($"Exceção: {ex.GetType().FullName} | " +
+                    $"Mensagem: {ex.Message}");
+}
+finally
+{
+    if (client is not null)
+    {
+        await client.CloseAsync();
+        logger.Information(
+            "Conexao com o Azure Service Bus fechada!");
+    }
+        
+}
